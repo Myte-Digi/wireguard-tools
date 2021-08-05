f6d1a633211091cc66c199e8a9fe41010aa41e91
Loi Tran <loi@anantadigi.com>
2021-08-05 16:38:11 +0700

    update

--- install_nginx.sh
+++ install_nginx.sh
@@ -9,6 +9,7 @@ nano /etc/nginx/nginx.conf
 # Load balance UDP‑based DNS traffic across two servers
 stream {
     upstream dns_upstreams {
+        hash $remote_addr consistent;
         server 167.71.208.53:50000;#vpn2
         server 188.166.241.118:50000; #vpn3
     }
@@ -16,7 +17,7 @@ stream {
     server {
         listen 51820 udp;
         proxy_pass dns_upstreams;
-        proxy_timeout 1s;
+        proxy_timeout 5s;
         proxy_responses 1;
         error_log /var/log/nginx/wireguard_error.log;
     }
